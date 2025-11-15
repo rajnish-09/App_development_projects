6505fd9736edb9947d9cf7ae08e3b5bd29ada393
rajnish-09 <rajnish9861@gmail.com>
2025-11-15 19:42:55 +0545

    data insertion functionality to register

--- KuraKani/app/src/main/java/com/example/kurakani/DbHelper.java
+++ KuraKani/app/src/main/java/com/example/kurakani/DbHelper.java
@@ -1,4 +1,51 @@
 package com.example.kurakani;
 
-public class DbHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private Context context;
+    private static final String Db_name = "kurakani.db";
+    private static final int Db_version = 1;
+    private static final String Table_name = "registration";
+//    private static
+
+    public DbHelper(@Nullable Context context) {
+        super(context, Db_name, null, Db_version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String qry = "CREATE TABLE "+Table_name+" (SNo INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Gmail TEXT, Number TEXT, Password TEXT, Gender TEXT)";
+        db.execSQL(qry);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1){
+        db.execSQL("DROP TABLE IF EXISTS "+Table_name);
+        onCreate(db);
+    }
+
+    public void registerPeople(String name, String gmail, String number, String password, String gender){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("Name", name);
+        cv.put("Gmail", gmail);
+        cv.put("Number", number);
+        cv.put("Password", password);
+        cv.put("Gender", gender);
+        Long res = db.insert(Table_name, null, cv);
+        if(res == -1){
+            Toast.makeText(context, "Failed to register. Try again", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(context, "Registered successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
--- KuraKani/app/src/main/java/com/example/kurakani/RegistrationActivity.java
+++ KuraKani/app/src/main/java/com/example/kurakani/RegistrationActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,16 +16,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrationActivity extends AppCompatActivity {
-    TextView login_link;
+    TextView name, gmail, number, password, reTypePassword, login_link;
+    Button register;
+    Intent intent;
+
+    RadioGroup group;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        name = findViewById(R.id.name);
+        gmail = findViewById(R.id.gmail);
+        number = findViewById(R.id.number);
+        password = findViewById(R.id.password);
+        reTypePassword = findViewById(R.id.retype_password);
         login_link = findViewById(R.id.login_link);
+        register = findViewById(R.id.register);
+        group = findViewById(R.id.group);
+
+        register.setOnClickListener(v->{
+            String nameInput = name.getText().toString();
+            String gmailInput = gmail.getText().toString();
+            String numberInput = number.getText().toString();
+            String passwordInput = password.getText().toString();
+
+            //getting selected radio button
+            int selectedId = group.getCheckedRadioButtonId();
+            RadioButton selectedRadioButton = findViewById(selectedId);
+            String selectedText = selectedRadioButton.getText().toString();
+
+            DbHelper myDB = new DbHelper(RegistrationActivity.this);
+            myDB.registerPeople(nameInput, gmailInput, numberInput, passwordInput, selectedText);
+        });
 
         login_link.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
